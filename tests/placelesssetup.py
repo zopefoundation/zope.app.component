e7a2b1ac400a7eb85fb3730857f33be6846daa6d
Sidnei da Silva <sidnei.da.silva@gmail.com>
2003-02-11 16:00:10 +0000

    Merging paris-copypasterename-branch. Not very fun :(

--- tests/placelesssetup.py
+++ tests/placelesssetup.py
@@ -13,7 +13,7 @@
 ##############################################################################
 """Unit test logic for setting up and tearing down basic infrastructure
 
-$Id: placelesssetup.py,v 1.3 2003/02/06 06:49:24 seanb Exp $
+$Id: placelesssetup.py,v 1.4 2003/02/11 15:59:40 sidnei Exp $
 """
 
 from zope.component import getServiceManager
