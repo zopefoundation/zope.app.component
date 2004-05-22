5c834e35d43a83d9aec271de9f8bd36eeabb4490
Albertas Agejevas <alga@pov.lt>
2004-05-22 01:56:54 +0000

    Made getServices() to use the site stored as a thread global for local services lookup.  ZopePublication sends events before traversing each object, and the site thread global is set by a subscriber to that event if the object being traversed is an ISite.

--- hooks.py
+++ hooks.py
@@ -16,10 +16,12 @@
 $Id$
 """
 
-from zope.component import getService
+import warnings
+from zope.component import getService, getAdapter, queryAdapter
 from zope.component.interfaces import IServiceService
 from zope.app.site.interfaces import ISite
 from zope.component.service import serviceManager
+from zope.component.exceptions import ComponentLookupError
 from zope.proxy import removeAllProxies
 from zope.security.proxy import trustedRemoveSecurityProxy
 from zope.app.traversing import IContainmentRoot
@@ -27,44 +29,34 @@ from zope.app.location.interfaces import ILocation
 from zope.app.location import locate
 from zope.component.servicenames import Presentation
 from zope.interface import Interface
+from zope.thread import thread_globals
 
-def getServiceManager_hook(context, local=False, recurse=False):
+def getServices_hook(context=None):
 
     if context is None:
+        site = thread_globals().site
+        if site is None:
+            return serviceManager
+        else:
+            return trustedRemoveSecurityProxy(site.getSiteManager())
+
+    try:
+        # This try-except is just backward compatibility really
+        return trustedRemoveSecurityProxy(getAdapter(context, IServiceService))
+    except ComponentLookupError:
+        # Deprecated support for a context that isn't adaptable to
+        # IServiceService.  Return the default service manager.
+        ## warnings.warn("getServices' context arg must be None or"
+        ##               "  adaptable to IServiceService.",
+        ##               DeprecationWarning, warningLevel())
         return serviceManager
 
-    clean_context = removeAllProxies(context)
 
-    # if the context is actually a service or site manager...
-    if IServiceService.providedBy(clean_context):
-        return trustedRemoveSecurityProxy(context)
-
-    elif (ISite.providedBy(clean_context)):
-        return trustedRemoveSecurityProxy(context.getSiteManager())
-    else:
-        container = getattr(context, '__parent__', None)
-        if container is None:
-            if local:
-                # Check to make sure that when we run out of context, we
-                # have a root object:
-                if not IContainmentRoot.providedBy(context):
-                    raise TypeError("Not enough context to get next "
-                                    "site manager")
-
-            # Fall back to global:
-            sm = serviceManager
-        else:
-            # We have a container, so context is a wrapper.  We still
-            # don't have a site manager, so try again, recursively:
-            sm = getServiceManager_hook(container, local, True)
-
-    return sm
-
-
-def queryView(object, name, request, default=None, context=None,
-              providing=Interface):
-    if context is None:
-        context = object
+def queryView(object, name, request, default=None,
+              providing=Interface, context=None):
+    # XXX test
+    #if context is None:
+    #    context = object
     views = getService(context, Presentation)
     view = views.queryView(object, name, request, default=default,
                            providing=providing)
