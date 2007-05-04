6a44211ed421a6ae59c831428dc64ac9ab0038f9
Christian Theune <ct@gocept.com>
2007-05-04 07:24:32 +0000

    Removed external to move code here. Updated version for *upcoming* release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages, Extension
 
 setup(name='zope.app.component',
-      version='0.1dev',
+      version = '3.4.0b1',
       url='http://svn.zope.org/zope.app.component',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
