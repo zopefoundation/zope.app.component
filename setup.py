b15a2f86447bea36d6896d052f153dc381b5ac81
Gediminas Paulauskas <menesis@pov.lt>
2010-09-01 10:44:59 +0000

    Preparing release 3.9.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.component',
-      version='3.9.1dev',
+      version='3.9.1',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Local Zope Component Support',
